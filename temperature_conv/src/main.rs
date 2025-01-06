1bfe1ce55520cafba2b1b0f8c6d643c75967662f
TimurKaiser <baran.aksoy.pro@gmail.com>
2025-01-06 16:32:33 +0100

    Temp conv

---
+++ temperature_conv/src/main.rs
@@ -0,0 +1,8 @@
+// // Here is a possible test for your function:
+
+use temperature_conv::*;
+
+fn main() {
+    println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
+    println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
+}
