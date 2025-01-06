f7e09071b37d644fcfe9565baec9dac6d1860842
TimurKaiser <baran.aksoy.pro@gmail.com>
2025-01-06 21:30:12 +0100

    find factorial

---
+++ find_factorial/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn factorial(num: u64) -> u64 {
+    let mut result = 1;
+
+    for i in 1..= num {
+        result = result * i;
+    }
+    result
+}
---
+++ find_factorial/src/main.rs
@@ -0,0 +1,11 @@
+// Here is a possible program to test your function:
+
+use find_factorial::*;
+
+fn main() {
+    println!("The factorial of 0 = {}", factorial(0));
+    println!("The factorial of 1 = {}", factorial(1));
+    println!("The factorial of 5 = {}", factorial(5));
+    println!("The factorial of 10 = {}", factorial(10));
+    println!("The factorial of 19 = {}", factorial(19));
+}
