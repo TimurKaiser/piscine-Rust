b58676de9fa96302da99ef4702c4e275bdb37c62
TimurKaiser <baran.aksoy.pro@gmail.com>
2025-01-06 15:37:18 +0100

    Error in quo and rem

---
+++ scalar/target/debug/libscalar.d
@@ -0,0 +1,1 @@
+/home/baran/Z01/Rust/piscine/scalar/target/debug/libscalar.rlib: /home/baran/Z01/Rust/piscine/scalar/src/lib.rs
---
+++ scalar/target/debug/scalar.d
@@ -0,0 +1,1 @@
+/home/baran/Z01/Rust/piscine/scalar/target/debug/scalar: /home/baran/Z01/Rust/piscine/scalar/src/lib.rs /home/baran/Z01/Rust/piscine/scalar/src/main.rs
