54121f9bf0a05739a21096c818db3194785f1773
TimurKaiser <baran.aksoy.pro@gmail.com>
2025-01-06 18:25:20 +0100

    clone and else is add

--- groceries/src/lib.rs
+++ groceries/src/lib.rs
@@ -1,10 +1,12 @@
 pub fn insert(vec: &mut Vec<String>, val: String) {
-    vec.push(val);
+    vec.push(val.clone());
     println!("{}", val);
 }
 
 pub fn at_index(slice: &[String], index: usize) -> &str {
     if index < slice.len() {
         &slice[index]
-    } 
+    } else {
+        panic!("Index big");
+    }
 }
