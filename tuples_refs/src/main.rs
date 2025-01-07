f325f14519436aa79493bf9884c4f372e8ba1bb8
TimurKaiser <baran.aksoy.pro@gmail.com>
2025-01-07 13:17:51 +0100

    tuples refs

---
+++ tuples_refs/src/main.rs
@@ -0,0 +1,10 @@
+// Here is a program to test your functions
+
+use tuples_refs::*;
+
+fn main() {
+    let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
+    println!("Student's first name: {}", first_name(&student));
+    println!("Student's last name: {}", last_name(&student));
+    println!("Student's id: {}", id(&student));
+}
