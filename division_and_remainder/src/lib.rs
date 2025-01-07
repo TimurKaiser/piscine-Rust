667fbb71d75c3e83f7c4ac60a380886018c29d80
TimurKaiser <baran.aksoy.pro@gmail.com>
2025-01-07 12:48:15 +0100

    division and remainder

---
+++ division_and_remainder/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn divide(x: i32, y: i32) -> (i32, i32) {
+    let division = x / y;
+    let remainder = x % y;
+
+    (division, remainder)
+
+}
--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,5 +1,5 @@
-#[derive(Debug)]
-pub struct Matrix(pub (i32, i32), pub (i32, i32)); // Ajout de `pub` devant chaque tuple
+#[derive(Debug, PartialEq, Eq)]
+pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
     Matrix((m.0 .0, m.1 .0), (m.0 .1, m.1 .1))
